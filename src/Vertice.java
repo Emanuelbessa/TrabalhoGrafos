e70888887ea2e7384fe21dfe377ff89d01b295e0
Murilo A. 🎧 <muriloandradealmeida@gmail.com>
2019-09-30 22:19:20 -0300

    Methods implemented for class Vertice

--- src/Vertice.java
+++ src/Vertice.java
@@ -1,3 +1,17 @@
+import java.util.Objects;
+
 public class Vertice {
     public String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean equals(final Vertice outroVertice) {
+        return Objects.equals(this.nome, outroVertice.nome);
+    }
 }
